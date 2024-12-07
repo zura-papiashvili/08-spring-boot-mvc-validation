5c4314d98b3043f4e78647839e0dceb9738e8988
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2024-12-06 19:20:34 -0500

    feat : custom errors

--- src/main/java/com/springcourse/springdemo/mvc/Customer.java
+++ src/main/java/com/springcourse/springdemo/mvc/Customer.java
@@ -3,6 +3,7 @@ package com.springcourse.springdemo.mvc;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class Customer {
@@ -17,7 +18,11 @@ public class Customer {
 
     @Min(value = 0, message = "must be greater than or equal to zero")
     @Max(value = 10, message = "must be less than or equal to 10")
-    private int freePasses;
+    @NotNull(message = "is required")
+    private Integer freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
 
     public Customer() {
     }
@@ -38,12 +43,20 @@ public class Customer {
         return lastName;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
     }
 
 }
